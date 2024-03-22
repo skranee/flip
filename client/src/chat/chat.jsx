394ff5945605f14d1096ed5a8119210a40e1c743
skranee <125743202+skranee@users.noreply.github.com>
2024-03-22 19:20:28 +0300

    deployment

--- client/src/chat/chat.jsx
+++ client/src/chat/chat.jsx
@@ -34,7 +34,7 @@ function Chat() {
 
     useEffect(() => {
         let pingInterval;
-        socket.current = new WebSocket('wss://localhost:4000');
+        socket.current = new WebSocket('ws://localhost:4000');
         function heartbeat() {
             socket.current.send(JSON.stringify({ type: 'pong' }));
         }
