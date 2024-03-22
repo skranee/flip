fd0352d884e51658b4c2898dbe63d2291237bdc1
skranee <125743202+skranee@users.noreply.github.com>
2024-03-23 01:32:09 +0300

    changes

--- client/src/chat/chat.jsx
+++ client/src/chat/chat.jsx
@@ -34,7 +34,7 @@ function Chat() {
 
     useEffect(() => {
         let pingInterval;
-        socket.current = new WebSocket("wss://mm2fliptest.ru:4000/");
+        socket.current = new WebSocket("wss://mm2fliptest.ru/ws");
         function heartbeat() {
             socket.current.send(JSON.stringify({ type: 'pong' }));
         }
