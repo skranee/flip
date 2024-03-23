0a4b74cee8d73c73651634335e68cd6d359c4247
skranee <125743202+skranee@users.noreply.github.com>
2024-03-23 05:18:34 +0300

    final

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {AuthResponse} from "../models/response/AuthResponse";
 
-export const API_URL = 'https://mm2fliptest.ru/api'
+export const API_URL = 'https://mm2flip.com/api'
 
 const $api = axios.create({
     withCredentials: true,
