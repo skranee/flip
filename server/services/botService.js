19ef1f507d60bfc9744ad7bc0c1d6da7f9733bd5
skranee <125743202+skranee@users.noreply.github.com>
2024-03-24 03:30:10 +0300

    changes

--- server/services/botService.js
+++ server/services/botService.js
@@ -138,34 +138,37 @@ class BotService {
             }
             user = await userModel.findOne({robloxId: robloxId});
         }
+        const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
         let newItems = [];
         for(const item of items) {
             for(let i = 0; i < item.quantity; ++i) {
                 const id = uuidv4();
                 const parsedInfo = await this.parseHtml(item.name, item);
                 const assetIdItem = parsedInfo.assetId;
+                await delay();
+                let imageResponse = {};
                 try {
-                    const imageResponse = await axios.get(`https://thumbnails.roblox.com/v1/assets?assetIds=${assetIdItem}&returnPolicy=PlaceHolder&size=700x700&format=Png&isCircular=false`);
-                    let itemImage = '';
-                    if(imageResponse && imageResponse.data) {
-                        itemImage = imageResponse.data.data[0].imageUrl;
-                    } else {
-                        itemImage = imageLink;
-                    }
-                    const add = await botModel.create(
-                        {
-                            name: parsedInfo.name,
-                            owner: user._id,
-                            image: itemImage,
-                            price: parsedInfo.price * process.env.CURRENCY_CONVERTER,
-                            itemId: id,
-                            holder: item.holder,
-                            gameName: item.gameName
-                        });
-                    newItems.push(add);
+                    imageResponse = await axios.get(`https://thumbnails.roblox.com/v1/assets?assetIds=${assetIdItem}&returnPolicy=PlaceHolder&size=700x700&format=Png&isCircular=false`);
                 } catch(e) {
                     console.log(e)
                 }
+                let itemImage = '';
+                if(imageResponse && imageResponse.data) {
+                    itemImage = imageResponse.data.data[0].imageUrl;
+                } else {
+                    itemImage = imageLink;
+                }
+                const add = await botModel.create(
+                    {
+                        name: parsedInfo.name,
+                        owner: user._id,
+                        image: itemImage,
+                        price: parsedInfo.price * process.env.CURRENCY_CONVERTER,
+                        itemId: id,
+                        holder: item.holder,
+                        gameName: item.gameName
+                    });
+                newItems.push(add);
             }
         }
         return newItems;
