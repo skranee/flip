3ffaa94b2264ba81a76796355536266e67932c7b
skranee <125743202+skranee@users.noreply.github.com>
2024-06-13 10:59:55 +0300

    changes

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {AuthResponse} from "../models/response/AuthResponse";
 
-export const API_URL = 'http://localhost:7000/api'
+export const API_URL = 'http://209.38.218.8:7000/api'
 
 const $api = axios.create({
     withCredentials: true,
